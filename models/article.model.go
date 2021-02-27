256f089c8c12242c8a683201bf33000fe886f268
Voratham (Dream) Siriapiwattanakul <voratham_sir@true-e-logistics.com>
2021-02-28 03:26:56 +0700

    📦 ⛏ Add script migration, Refactor articles controller, Fix update article api

--- models/article.model.go
+++ models/article.model.go
@@ -4,8 +4,11 @@ import "gorm.io/gorm"
 
 type Article struct {
 	gorm.Model
-	Title   string `gorm:"unique;not null"`
-	Excerpt string `gorm:"not null"`
-	Body    string `gorm:"not null"`
-	Image   string `gorm:"not null"`
+	Title      string `gorm:"unique;not null"`
+	Excerpt    string `gorm:"not null"`
+	Body       string `gorm:"not null"`
+	Image      string `gorm:"not null"`
+	CategoryID int
+	// gorm library checking foreignKey has on category table
+	Category Category `gorm:"foreignKey:CategoryID"`
 }
